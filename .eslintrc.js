80a6e9ba9f85d7d4e02e89f5c1eb260bd0ecd09a
Kemal Ahmed <goatandsheep@gmail.com>
2020-04-09 14:03:14 -0400

    superstrict linting

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    'env': {
+        'node': true,
+        'es6': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'react-app',
+        'google',
+    ],
+    'parserOptions': {
+      'ecmaVersion': 2018,
+      'sourceType': 'module',
+    },
+    'parserOptions': {
+      'ecmaVersion': 2018,
+      'sourceType': 'module',
+    },
+    'rules': {
+      'linebreak-style': 0
+    }
+  };
+  
--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
-// import './App.css';
 import './App.scss';
+import {BrowserRouter as Router} from 'react-router-dom';
+import Route from 'react-router-dom/Route';
 
+/**
+ * Main App builder
+ * @return {Object} reactDOM
+ */
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header >
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </Router>
   );
 }
 
