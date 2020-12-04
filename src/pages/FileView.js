10c10277399a818a0c8b93512bf2c4d96e827c09
jiwan <jiwanjotsingh07@gmail.com>
2020-12-04 11:30:53 -0500

    add multiple resizable/dragable blocks and fixed overlay issue

--- src/pages/FileView.js
+++ src/pages/FileView.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React, {useContext, useEffect, useState, useRef} from 'react';
 import { Rnd } from "react-rnd";
 import {GlobalContext} from '../contexts/GlobalState';
 import constants from '../constants';
@@ -23,12 +23,25 @@ const FileView = (props) => {
     y: 10
   });
 
-  // this.state = {
-  //   width: 200,
-  //   height: 200,
-  //   x: 10,
-  //   y: 10
-  // };
+  const [rnd2, setRnd2] = useState({
+    width: 200,
+    height: 200,
+    x: 50,
+    y: 50
+  });
+
+  const [rnd3, setRnd3] = useState({
+    width: 200,
+    height: 200,
+    x: 150,
+    y: 150
+  });
+
+  const rndManagerRef = useRef({
+    maxZIndex: "999",
+    prevDraggedNode: null,
+    prevDraggedNodeZIndex: null
+  });
 
   useEffect(() => {
     const fetchData = async () => {
@@ -44,8 +57,16 @@ const FileView = (props) => {
   }, [globalConsumer, props.id]);
 
   useEffect(() => {
-    console.log('X and Y values', rnd);
+    console.log('first(1) rnd- X and Y values', rnd);
   }, [rnd]);
+
+  useEffect(() => {
+    console.log('second(2) rnd- X and Y values', rnd2);
+  }, [rnd2]);
+
+  useEffect(() => {
+    console.log('third(3) rnd- X and Y values', rnd3);
+  }, [rnd3]);
 
   return (
     <div>
@@ -97,9 +118,73 @@ const FileView = (props) => {
             ...position
           });
         }}
+        onDragStart={(e, node) => {
+          const manager = rndManagerRef.current;
+          if (manager.prevDraggedNode) {
+            manager.prevDraggedNode.style.zIndex = manager.prevDraggedNodeZIndex;
+          }
+          manager.prevDraggedNode = node.node;
+          manager.prevDraggedNodeZIndex = manager.prevDraggedNode.style.zIndex;
+          manager.prevDraggedNode.style.zIndex = manager.maxZIndex;
+        }}
         >
         Rnd
       </Rnd>
+
+      <Rnd
+        style={style}
+        size={{ width: rnd2.width, height: rnd2.height }}
+        position={{ x: rnd2.x, y: rnd2.y }}
+        onDragStop={(e, d) => {
+          setRnd2({ x: d.x, y: d.y });
+        }}
+        onResizeStop={(e, direction, ref, delta, position) => {
+          setRnd2({
+            width: ref.style.width,
+            height: ref.style.height,
+            ...position
+          });
+        }}
+        onDragStart={(e, node) => {
+          const manager = rndManagerRef.current;
+          if (manager.prevDraggedNode) {
+            manager.prevDraggedNode.style.zIndex = manager.prevDraggedNodeZIndex;
+          }
+          manager.prevDraggedNode = node.node;
+          manager.prevDraggedNodeZIndex = manager.prevDraggedNode.style.zIndex;
+          manager.prevDraggedNode.style.zIndex = manager.maxZIndex;
+        }}
+        >
+        Rnd2
+      </Rnd>
+
+      <Rnd
+        style={style}
+        size={{ width: rnd3.width, height: rnd3.height }}
+        position={{ x: rnd3.x, y: rnd3.y }}
+        onDragStop={(e, d) => {
+          setRnd3({ x: d.x, y: d.y });
+        }}
+        onResizeStop={(e, direction, ref, delta, position) => {
+          setRnd3({
+            width: ref.style.width,
+            height: ref.style.height,
+            ...position
+          });
+        }}
+        onDragStart={(e, node) => {
+          const manager = rndManagerRef.current;
+          if (manager.prevDraggedNode) {
+            manager.prevDraggedNode.style.zIndex = manager.prevDraggedNodeZIndex;
+          }
+          manager.prevDraggedNode = node.node;
+          manager.prevDraggedNodeZIndex = manager.prevDraggedNode.style.zIndex;
+          manager.prevDraggedNode.style.zIndex = manager.maxZIndex;
+        }}
+        >
+        Rnd3
+      </Rnd>
+      
     </div>
   );
 };
