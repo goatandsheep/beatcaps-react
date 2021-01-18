29e1ea76795041274b5ceb174b3445e917b3a3b7
Kemal Ahmed <goatandsheep@gmail.com>
2021-01-18 15:56:58 -0500

    templates are moveable

--- src/components/TemplateDragDrop.js
+++ src/components/TemplateDragDrop.js
@@ -1,136 +1,58 @@
-import React, {useEffect, useState, useRef} from 'react';
-import { Rnd } from "react-rnd";
+import React, {useRef} from 'react';
+import {Rnd} from 'react-rnd';
 
 const dropboxStyles = {
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-  border: "solid 1px #ddd",
-  background: "#f0f0f0"
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  border: 'solid 1px #ddd',
+  background: '#f0f0f0',
 };
 
-const TemplateDragDrop = () => {
-  const [rnd, setRnd] = useState({
-    width: 200,
-    height: 200,
-    x: 10,
-    y: 10
-  });
-
-  const [rnd2, setRnd2] = useState({
-    width: 200,
-    height: 200,
-    x: 50,
-    y: 50
-  });
-
-  const [rnd3, setRnd3] = useState({
-    width: 200,
-    height: 200,
-    x: 150,
-    y: 150
-  });
-
+const TemplateDragDrop = ({
+  handleDragDropResize,
+  viewOptions,
+  height,
+  width,
+  handleXYChange,
+}) => {
   const rndManagerRef = useRef({
     maxZIndex: "999",
     prevDraggedNode: null,
-    prevDraggedNodeZIndex: null
+    prevDraggedNodeZIndex: null,
   });
-
-  useEffect(() => {
-    console.log('first(1) rnd- X and Y values', rnd);
-  }, [rnd]);
-
-  useEffect(() => {
-    console.log('second(2) rnd- X and Y values', rnd2);
-  }, [rnd2]);
-
-  useEffect(() => {
-    console.log('third(3) rnd- X and Y values', rnd3);
-  }, [rnd3]);
-
+  console.log(width)
   return (
     <>
-      <Rnd
-        style={dropboxStyles}
-        size={{ width: rnd.width, height: rnd.height }}
-        position={{ x: rnd.x, y: rnd.y }}
-        onDragStop={(e, d) => {
-          setRnd({ x: d.x, y: d.y });
-        }}
-        onResizeStop={(e, direction, ref, delta, position) => {
-          setRnd({
-            width: ref.style.width,
-            height: ref.style.height,
-            ...position
-          });
-        }}
-        onDragStart={(e, node) => {
-          const manager = rndManagerRef.current;
-          if (manager.prevDraggedNode) {
-            manager.prevDraggedNode.style.zIndex = manager.prevDraggedNodeZIndex;
-          }
-          manager.prevDraggedNode = node.node;
-          manager.prevDraggedNodeZIndex = manager.prevDraggedNode.style.zIndex;
-          manager.prevDraggedNode.style.zIndex = manager.maxZIndex;
-        }}
-        >
-        View 1
-      </Rnd>
+    {console.log('updating dragdrop', width)}
+    <p>{width} width</p>
+      {viewOptions.map((view, i) => {
+        const viewNum = i + 1;
 
-      <Rnd
-        style={dropboxStyles}
-        size={{ width: rnd2.width, height: rnd2.height }}
-        position={{ x: rnd2.x, y: rnd2.y }}
-        onDragStop={(e, d) => {
-          setRnd2({ x: d.x, y: d.y });
-        }}
-        onResizeStop={(e, direction, ref, delta, position) => {
-          setRnd2({
-            width: ref.style.width,
-            height: ref.style.height,
-            ...position
-          });
-        }}
-        onDragStart={(e, node) => {
-          const manager = rndManagerRef.current;
-          if (manager.prevDraggedNode) {
-            manager.prevDraggedNode.style.zIndex = manager.prevDraggedNodeZIndex;
-          }
-          manager.prevDraggedNode = node.node;
-          manager.prevDraggedNodeZIndex = manager.prevDraggedNode.style.zIndex;
-          manager.prevDraggedNode.style.zIndex = manager.maxZIndex;
-        }}
+        return (<Rnd
+          style={dropboxStyles}
+          bounds="#template-drag-drop-container"
+          size={{
+            width: viewOptions[i].width || (viewOptions[i].height * 16) / 9,
+            height: viewOptions[i].height,
+          }}
+          position={{ x: viewOptions[i].x, y: viewOptions[i].y }}
+          onDragStop={(e, d) => {
+            handleXYChange(viewNum, { x: d.x, y: d.y });
+          }}
+          onResizeStop={(e, direction, ref, delta, position) => {
+            // Note: have to set a new X and Y after resize, otherwise the box "jumps" if user drags from top or left corners
+            handleDragDropResize(viewNum, {
+              width: +ref.style.width.replace("px", ""),
+              height: +ref.style.height.replace("px", ""),
+              x: position.x,
+              y: position.y,
+            });
+          }}
         >
-        View 2
-      </Rnd>
-
-      <Rnd
-        style={dropboxStyles}
-        size={{ width: rnd3.width, height: rnd3.height }}
-        position={{ x: rnd3.x, y: rnd3.y }}
-        onDragStop={(e, d) => {
-          setRnd3({ x: d.x, y: d.y });
-        }}
-        onResizeStop={(e, direction, ref, delta, position) => {
-          setRnd3({
-            width: ref.style.width,
-            height: ref.style.height,
-            ...position
-          });
-        }}
-        onDragStart={(e, node) => {
-          const manager = rndManagerRef.current;
-          if (manager.prevDraggedNode) {
-            manager.prevDraggedNode.style.zIndex = manager.prevDraggedNodeZIndex;
-          }
-          manager.prevDraggedNode = node.node;
-          manager.prevDraggedNodeZIndex = manager.prevDraggedNode.style.zIndex;
-          manager.prevDraggedNode.style.zIndex = manager.maxZIndex;
-        }}
-        >
-        View 3
-      </Rnd>
+          View {viewNum}
+        </Rnd>)
+      })}
     </>
   );
 };
--- src/pages/TemplateDesigner.js
+++ src/pages/TemplateDesigner.js
@@ -1,49 +1,52 @@
-import React, {useContext, useState} from 'react';
+import React, {useContext, useState, useEffect} from 'react';
 import {GlobalContext} from '../contexts/GlobalState';
 import constants from '../constants';
-import TemplateDragDrop from '../components/TemplateDragDrop'
+import TemplateDragDrop from '../components/TemplateDragDrop';
 
 // styles
 const marginTop = {
-  marginTop: '1.5rem'
+  marginTop: '1.5rem',
 };
 const formStyles = {
-  marginBottom: '50px'
+  marginBottom: '50px',
+  overflowX: 'scroll'
 };
-const dragDropContainerStyles = {
-  height: '400px'
-};
-const viewInputStyles = {
-  width: 'auto',
-  minWidth: '75%'
-};
-const viewInputsGroupStyles = {
-  display: 'flex',
-  gap: '1rem'
-};
-const viewInputCol = {
-  width: '50%'
+
+const dragDropContainerStyles = (height,width) => {
+  return {
+    height: height + 'px',
+    width: width + 'px',
+    border: "1px solid darkgray",
+    background: "lightgray",
+  };
 };
 
 // constants
 const DEFAULT_VIEW_OBJECT = {
-  height: null,
-  width: null,
-  x: null,
-  y: null
-}
+  height: 250,
+  x: 0,
+  y: 0,
+};
+const DEFAULT_TEMPLATE_OBJECT = {
+  name: '',
+  height: 720,
+};
 
 const TemplateDesigner = () => {
   const globalConsumer = useContext(GlobalContext);
 
   const [viewOptions, setViewOptions] = useState([
-  DEFAULT_VIEW_OBJECT, DEFAULT_VIEW_OBJECT
-]);
+    DEFAULT_VIEW_OBJECT,
+    DEFAULT_VIEW_OBJECT,
+  ]);
+  const [templateOptions, setTemplateOptions] = useState(DEFAULT_TEMPLATE_OBJECT);
 
   const uploadTemplate = async (req) => {
+    const templateReq = templateOptions;
+    templateReq.views = viewOptions;
     const response = await fetch(`${constants.SERVER_DOMAIN}/templates/new`, {
       method: 'POST',
-      body: viewOptions, // TODO: Send correct format
+      body: JSON.stringify(templateReq), // TODO: Send correct format
       headers: {
         Authorization: globalConsumer.user.token,
       },
@@ -53,41 +56,67 @@ const TemplateDesigner = () => {
   };
 
   const handleViewOptionChange = (
-    viewNum,
-    field,
-    value
+      viewNum,
+      field,
+      value,
   ) => {
-    const newOptions = [...viewOptions]
-    const viewIndex = viewNum - 1
+    const newOptions = [...viewOptions];
 
+    const viewIndex = viewNum - 1;
     newOptions[viewIndex] = {
       ...viewOptions[viewIndex],
-      [field]: +value
-    }
+      [field]: +value,
+    };
+    
+    setViewOptions(newOptions);
+  };
 
-    setViewOptions(newOptions)
-  }
+  const handleXYChange = (viewNum, newXYViewOptionObj) => {
+    const newOptions = [...viewOptions];
+    const viewIndex = viewNum - 1;
+    newOptions[viewIndex] = {
+      ...viewOptions[viewIndex],
+      ...newXYViewOptionObj,
+    };
+
+    setViewOptions(newOptions);
+  };
+
+  const handleDragDropResize = (viewNum, newViewOptionObj) => {
+    const newOptions = [...viewOptions];
+    const viewIndex = viewNum - 1;
+    newOptions[viewIndex] = newViewOptionObj;
+
+    setViewOptions(newOptions);
+  };
+
+  const handleTemplateOptionChange = (field, value) => {
+    const newOptions = templateOptions;
+    newOptions[field] = value;
+    console.log("handleTemplateOptionChange", field, value, newOptions);
+    setTemplateOptions(newOptions);
+  };
 
   const handleNumberOfViewsChange = (newNumOfViews) => {
-    const newViewOptions = [...viewOptions]
+    const newViewOptions = [...viewOptions];
 
     // recursive function to remove/add objects to have the right number of objects
     const makeNewOptionsArray = () => {
       if (newNumOfViews > newViewOptions.length) {
-        newViewOptions.push(DEFAULT_VIEW_OBJECT)
-        makeNewOptionsArray()
+        newViewOptions.push(DEFAULT_VIEW_OBJECT);
+        makeNewOptionsArray();
       } else if (newNumOfViews < newViewOptions.length) {
-        newViewOptions.pop()
-        makeNewOptionsArray()
+        newViewOptions.pop();
+        makeNewOptionsArray();
       }
       return;
-    }
+    };
 
     makeNewOptionsArray();
-    setViewOptions(newViewOptions)
-  }
+    setViewOptions(newViewOptions);
+  };
 
-  const handleSubmit =  async (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
     await uploadTemplate(event.target);
 
@@ -95,82 +124,191 @@ const TemplateDesigner = () => {
   };
 
   const makeViewOptionInputs = () => {
-    const fieldsets = []
+    const fieldsets = [];
 
     for (let viewNum = 1; viewNum <= viewOptions.length; viewNum++) {
-      const currentValues = viewOptions[viewNum - 1]
+      const currentValues = viewOptions[viewNum - 1];
 
       fieldsets.push(
-        <fieldset className="field card">
-          <div className="card-content">
-            <h3 className="is-size-5">View {viewNum} Options</h3>
-            <div style={viewInputsGroupStyles}>
-              <div style={viewInputCol}>
-                <label htmlFor="viewHeight" className="label">Height</label>
-                <input onChange={(evt) => handleViewOptionChange(viewNum, 'height', evt.target.value)} id="viewHeight" className="input" type="number" style={viewInputStyles} value={currentValues.height}/>
-                <label htmlFor="viewWidth" className="label">Width</label>
-                <input onChange={(evt) => handleViewOptionChange(viewNum, 'width', evt.target.value)} id="viewWidth" className="input" type="number" style={viewInputStyles} value={currentValues.width}/>
-              </div>
-              <div style={viewInputCol}>
-                <label htmlFor="viewX" className="label">X Coordinate</label>
-                <input onChange={(evt) => handleViewOptionChange(viewNum, 'x', evt.target.value)} id="viewX" className="input" type="number" style={viewInputStyles} value={currentValues.x}/>
-                <label htmlFor="viewY" className="label">Y Coordinate</label>
-                <input onChange={(evt) => handleViewOptionChange(viewNum, 'y', evt.target.value)} id="viewY" className="input" type="number" style={viewInputStyles} value={currentValues.y}/>
+          <fieldset className="field card">
+            <div className="card-content">
+              <h3 className="is-size-5">View {viewNum} Options</h3>
+              <div className="columns is-desktop">
+                <div className="column">
+                  <div className="field">
+                    <label htmlFor="viewHeight" className="label">Height</label>
+                    <div className="control is-expanded has-icons-left">
+                    <input onChange={(evt) => handleViewOptionChange(viewNum, 'height', +evt.target.value)} id={'viewHeight' + viewNum} className="input" required type="number" value={currentValues.height}/>
+                      <span className="icon is-small is-left">
+                        <i className="fas fa-ruler-vertical"></i>
+                      </span>
+                    </div>
+                  </div>
+                  <div className="field">
+                    <label htmlFor="viewWidth" className="label">Width</label>
+                    <div className="control is-expanded has-icons-left">
+                      <input onChange={(evt) => {handleViewOptionChange(viewNum, 'width', +evt.target.value)
+                    }} id={'viewWidth' + viewNum} className="input" type="number" value={currentValues.width}/>
+                      <span class="icon is-small is-left">
+                        <i class="fas fa-ruler-horizontal"></i>
+                      </span>
+                    </div>
+                    <p className="help">Leave blank to use 16:9 aspect ratio</p>
+                  </div>
+                </div>
+                <div className="column">
+                  <div className="field">
+                    <label htmlFor="viewX" className="label">X Coordinate</label>
+                    <div className="control is-expanded has-icons-left">
+                      <input onChange={(evt) => handleViewOptionChange(viewNum, 'x', evt.target.value)} id={'viewX' + viewNum} className="input" required type="number" value={currentValues.x}/>
+                      <span class="icon is-small is-left">
+                        <i class="fas fa-arrows-alt-h"></i>
+                      </span>
+                    </div>
+                  </div>
+                  <div className="field">
+                    <label htmlFor="viewY" className="label">Y Coordinate</label>
+                    <div className="control is-expanded has-icons-left">
+                      <input onChange={(evt) => handleViewOptionChange(viewNum, 'y', evt.target.value)} id={'viewY' + viewNum} className="input" required type="number" value={currentValues.y}/>
+                      <span class="icon is-small is-left">
+                        <i class="fas fa-arrows-alt-v"></i>
+                      </span>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </fieldset>
-      )
+          </fieldset>,
+      );
     }
 
     return fieldsets;
-  }
-  
+  };
+
   return (
     <div>
       <h1 className="title is-1">Template Designer</h1>
-      <form className="card has-text-left" onSubmit={handleSubmit} style={formStyles}>
+      <form
+        className="card has-text-left"
+        onSubmit={handleSubmit}
+        style={formStyles}
+      >
         <div className="card-content card">
           <div className="field">
-            <label htmlFor="viewName" className="label">Template Name</label>
-            <div className="control">
-              <input id="viewName" className="input" type="text" style={viewInputStyles}/>
+            <label htmlFor="viewName" className="label">
+              Template Name
+            </label>
+            <div className="control is-expanded has-icons-left">
+              <input
+                onChange={(e) =>
+                  handleTemplateOptionChange("name", e.target.value)
+                }
+                id="viewName"
+                className="input"
+                required
+                type="text"
+                value={templateOptions.name}
+              />
+              <span class="icon is-small is-left">
+                <i class="fas fa-signature"></i>
+              </span>
             </div>
           </div>
           <div className="field">
-            <label htmlFor="viewOutputHeight" className="label">Template Height</label>
-            <div className="control">
-              <input id="viewOutputHeight" className="input" type="number" style={viewInputStyles}/>
+            <label htmlFor="viewOutputHeight" className="label">
+              Template Height
+            </label>
+            <div className="control is-expanded has-icons-left">
+              <input
+                onChange={(e) =>
+                  handleTemplateOptionChange("height", +e.target.value)
+                }
+                id="viewOutputHeight"
+                className="input"
+                required
+                type="number"
+                value={templateOptions.height}
+              />
+              <span class="icon is-small is-left">
+                <i class="fas fa-ruler-vertical"></i>
+              </span>
             </div>
           </div>
           <div className="field">
-            <label htmlFor="viewOutputWidth" className="label">Template Width</label>
-            <div className="control">
-              <input id="viewOutputWidth" className="input" type="number" style={viewInputStyles}/>
+            <label htmlFor="viewOutputWidth" className="label">
+              Template Width
+            </label>
+            <div className="control is-expanded has-icons-left">
+              <input
+                onChange={(e) =>
+                  handleTemplateOptionChange("width", +e.target.value)
+                }
+                id="viewOutputWidth"
+                className="input"
+                type="number"
+                value={templateOptions.width}
+              />
+              <span class="icon is-small is-left">
+                <i class="fas fa-ruler-horizontal"></i>
+              </span>
             </div>
+            <p className="help">Leave blank to use 16:9 aspect ratio</p>
+            <button
+              onClick={(e) => handleTemplateOptionChange("width", "")}
+              className="button is-info"
+              type="button"
+            >
+              Clear
+            </button>
           </div>
-          <label htmlFor="templateName" className="label">Number of videos in this template</label>
-          <div className="control">
-            <div id="templateName" className="select">        
-                <select onChange={(e) => handleNumberOfViewsChange(e.target.value)} id="templateName">
+          <div className="field">
+            <label htmlFor="templateName" className="label">
+              Number of videos in this template
+            </label>
+            <div className="control is-expanded has-icons-left">
+              <div id="templateName" className="select">
+                <select
+                  onChange={(e) => handleNumberOfViewsChange(e.target.value)}
+                  id="templateName"
+                  required
+                  value={viewOptions.length}
+                >
                   <option value={2}>2</option>
                   <option value={3}>3</option>
                   <option value={4}>4</option>
                   <option value={5}>5</option>
                 </select>
+              </div>
+              <span class="icon is-small is-left">
+                <i class="fas fa-list"></i>
+              </span>
             </div>
+          </div>
+          <h2 className="title is-4" style={marginTop}>
+            Define Views
+          </h2>
+
+          <h2 className="title is-4" style={marginTop}>
+            Drag and Drop Editor
+          </h2>
+            <div
+            style={dragDropContainerStyles(templateOptions.height, templateOptions.width || templateOptions.height * 16 / 9)}
+            id="template-drag-drop-container"
+          >
+            <TemplateDragDrop
+              handleDragDropResize={handleDragDropResize}
+              handleXYChange={handleXYChange}
+              viewOptions={viewOptions}
+              height={templateOptions.height}
+              width={templateOptions.width || (templateOptions.height * 16) / 9}
+            />
           </div>
 
-          <h2 className="title is-4" style={marginTop}>Define Views</h2>
           {makeViewOptionInputs(viewOptions)}
-          
-          <h2 className="title is-4" style={marginTop}>Drag and Drop Editor</h2>
-          <div class="card" style={dragDropContainerStyles}>
-            <div className="card-container">
-              <TemplateDragDrop />
-            </div>
-          </div>
-          <button type="submit" className="button is-primary" style={marginTop}>Create Template</button>
+
+          <button type="submit" className="button is-primary" style={marginTop}>
+            Create Template
+          </button>
         </div>
       </form>
     </div>
