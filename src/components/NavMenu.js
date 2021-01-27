0fef51fb911c28cbadc32daa9a6723220cad179b
Linda <lindajzhao@gmail.com>
2021-01-26 22:36:05 -0500

    style with bulma

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -4,39 +4,70 @@ import AuthButton from './AuthButton';
 
 const NavMenu = () => {
   return (
-    <nav className="navbar" role="navigation" aria-label="main navigation">
-      <div className="navbar-brand">
-        <NavLink className="navbar-item title is-6" to="/">BeatCaps Admin Panel</NavLink>
-      </div>
-      <div class="dropdown is-hoverable">
-        <div class="dropdown-trigger">
-          <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
-            <span>Dropdown button</span>
-            <span class="icon is-small">
-              <i class="fas fa-angle-down" aria-hidden="true"></i>
-            </span>
-          </button>
-        </div>
-        <div class="dropdown-menu" id="dropdown-menu" role="menu">
-          <div class="dropdown-content">
-            <a href="#" class="dropdown-item is-hoverable">
-              Dropdown item
-            </a>
-            <a class="dropdown-item is-hoverable">
-              Other dropdown item
-            </a>
-            <a href="#" class="dropdown-item is-hoverable">
-              Active dropdown item
-            </a>
-            <a href="#" class="dropdown-item is-hoverable">
-              Other dropdown item
-            </a>
+    <nav role="navigation" aria-label="main navigation">
+      <div className="is-flex is-justify-content-space-between is-align-items-center is-flex-wrap-wrap mx-6 my-4">
+        <div className="is-flex is-justify-content-flex-start is-align-items-center is-flex-wrap-wrap">
+          <div className="mr-4">
+            <NavLink className="title is-6" to="/">BeatCaps Admin Panel</NavLink>
+          </div>
+          
+          <div>
+            <div class="dropdown is-hoverable">
+              <div class="dropdown-trigger">
+                <button class="button is-white" aria-haspopup="true" aria-controls="dropdown-menu">
+                  <span>Overleia</span>
+                  <span class="icon is-small">
+                    <i class="fas fa-angle-down" aria-hidden="true"></i>
+                  </span>
+                </button>
+              </div>
+              <div class="dropdown-menu" id="dropdown-menu" role="menu">
+                <div class="dropdown-content">
+                  <Link to="/" class="dropdown-item is-hoverable">
+                    Overleia Outputs
+                  </Link>
+                  <hr class="dropdown-divider"></hr>
+                  <Link to="/file/new" class="dropdown-item is-hoverable">
+                    Upload Video
+                  </Link>
+                </div>
+              </div>
+            </div>
+
+            <div class="dropdown is-hoverable">
+              <div class="dropdown-trigger">
+                <button class="button is-white" aria-haspopup="true" aria-controls="dropdown-menu">
+                  <span>BeatCaps</span>
+                  <span class="icon is-small">
+                    <i class="fas fa-angle-down" aria-hidden="true"></i>
+                  </span>
+                </button>
+              </div>
+              <div class="dropdown-menu" id="dropdown-menu" role="menu">
+                <div class="dropdown-content">
+                  <Link to="/" class="dropdown-item is-hoverable">
+                    Video Outputs
+                  </Link>
+                  <hr class="dropdown-divider"></hr>
+                  <Link to="/file/new" class="dropdown-item is-hoverable">
+                    Upload Video
+                  </Link>
+                  <hr class="dropdown-divider"></hr>
+                  <Link to="/templates" class="dropdown-item is-hoverable">
+                    Templates List
+                  </Link>
+                  <Link to="/templates/new" class="dropdown-item is-hoverable">
+                    Create Template
+                  </Link>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="navbar-end">
-        <AuthButton />
+        <div className="is-flex is-align-items-center">
+          <AuthButton />
+        </div>
       </div>
     </nav>
   );
