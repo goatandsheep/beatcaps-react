d888cb306e77ea202686719f8d8cc19d89c2955e
lindajzhao <18090934+lindajzhao@users.noreply.github.com>
2021-01-28 16:54:33 -0500

    Menu links (#77)
    * move current menu items to NavMenu
    
    * style with bulma
    
    * remove errors

---
+++ src/components/NavMenu.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import {Link, NavLink} from 'react-router-dom';
+import AuthButton from './AuthButton';
+
+const NavMenu = () => {
+  return (
+    <nav role="navigation" aria-label="main navigation">
+      <div className="is-flex is-justify-content-space-between is-align-items-center is-flex-wrap-wrap mx-6 my-4">
+        <div className="is-flex is-justify-content-flex-start is-align-items-center is-flex-wrap-wrap">
+          <div className="mr-4">
+            <NavLink className="title is-6" to="/">BeatCaps Admin Panel</NavLink>
+          </div>
+          
+          <div>
+            <div className="dropdown is-hoverable">
+              <div className="dropdown-trigger">
+                <button className="button is-white" aria-haspopup="true" aria-controls="dropdown-menu">
+                  <span>Overleia</span>
+                  <span className="icon is-small">
+                    <i className="fas fa-angle-down" aria-hidden="true"></i>
+                  </span>
+                </button>
+              </div>
+              <div className="dropdown-menu" id="dropdown-menu" role="menu">
+                <div className="dropdown-content">
+                  <Link to="/" className="dropdown-item is-hoverable">
+                    Overleia Outputs
+                  </Link>
+                  <hr className="dropdown-divider"></hr>
+                  <Link to="/file/new" className="dropdown-item is-hoverable">
+                    Upload Video
+                  </Link>
+                </div>
+              </div>
+            </div>
+
+            <div className="dropdown is-hoverable">
+              <div className="dropdown-trigger">
+                <button className="button is-white" aria-haspopup="true" aria-controls="dropdown-menu">
+                  <span>BeatCaps</span>
+                  <span className="icon is-small">
+                    <i className="fas fa-angle-down" aria-hidden="true"></i>
+                  </span>
+                </button>
+              </div>
+              <div className="dropdown-menu" id="dropdown-menu" role="menu">
+                <div className="dropdown-content">
+                  <Link to="/" className="dropdown-item is-hoverable">
+                    Video Outputs
+                  </Link>
+                  <hr className="dropdown-divider"></hr>
+                  <Link to="/file/new" className="dropdown-item is-hoverable">
+                    Upload Video
+                  </Link>
+                  <hr className="dropdown-divider"></hr>
+                  <Link to="/templates" className="dropdown-item is-hoverable">
+                    Templates List
+                  </Link>
+                  <Link to="/templates/new" className="dropdown-item is-hoverable">
+                    Create Template
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="is-flex is-align-items-center">
+          <AuthButton />
+        </div>
+      </div>
+    </nav>
+  );
+};
+export default NavMenu;
