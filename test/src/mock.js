e72ebaef227ce1d35060cb4e90a0cd8d925ba3ae
Linda <lindajzhao@gmail.com>
2021-02-02 17:09:29 -0500

    remove login form and endpoint in mocks

--- test/src/mock.js
+++ test/src/mock.js
@@ -14,9 +14,6 @@ const Element = require('./mocks/Element.json')
 const ElementResponse = require('./mocks/ElementResponse.json')
 const ElementListResponse = require('./mocks/ElementListResponse.json')
 
-const LoginModel = require('./mocks/LoginModel.json')
-const LoginResponseModel = require('./mocks/LoginResponseModel.json')
-
 const OutputResponse = require('./mocks/OutputResponse.json')
 const OutputResponseList = require('./mocks/OutputResponseList.json')
 const Template = require('./mocks/Template.json')
@@ -30,8 +27,6 @@ const jwt = require('jsonwebtoken')
 
 const app = jsonServer.create()
 
-let router = jsonServer.router({})
-
 const mockRefs = [
   Element,
   ElementResponse,
@@ -43,26 +38,6 @@ const mockRefs = [
   TemplateResponseList,
   View,
 ]
-
-const SECRET_KEY = '123456789'
-const expiresIn = '1h'
-
-// Create a token from a payload
-function createToken(payload) {
-  return jwt.sign(payload, SECRET_KEY, { expiresIn })
-}
-
-// Verify the token
-function verifyToken(token) {
-  return jwt.verify(token, SECRET_KEY, (err, decode) => decode !== undefined ? decode : err)
-}
-
-// Check if the user exists in database
-function isAuthenticated({ username, password }) {
-  return (username && password)
-  // return userdb.users.findIndex(user => user.username === 'user@example.com' && user.password === 'password') !== -1
-}
-const middlewares = jsonServer.defaults()
 
 app.use((req, res, next) => {
   res.header('Allow')
@@ -80,25 +55,6 @@ app.use(bodyParser.json())
 
 app.options('/*', (req, res) => {
   res.status(200).jsonp({})
-})
-
-// app.use(/^(?!\/auth).*$/, (req, res, next) => {
-app.use(/^(?!\/login).*$/, (req, res, next) => {
-  if (req.headers.authorization === undefined || req.headers.authorization.split(' ')[0] !== 'Bearer') {
-    const status = 401
-    const message = 'Bad authorization header'
-    console.log(req.headers.authorization)
-    res.status(status).json({ status, message })
-    return
-  }
-  try {
-    verifyToken(req.headers.authorization.split(' ')[1])
-    next()
-  } catch (err) {
-    const status = 401
-    const message = 'Error: access_token is not valid'
-    res.status(status).json({ status, message })
-  }
 })
 
 /**
@@ -184,35 +140,7 @@ app.get('/file', (req, res) => {
   res.status(200).jsonp(rand)
 })
 
-// app.post('/login', (req, res) => {
-//   const rand = jsf.generate(LoginResponseModel)
-//   res.status(200).jsonp(rand)
-// })
-
 // keep at the bottom
-
-app.post('/auth/logout', (req, res) => {
-  const rand = jsf.generate(LoginModel)
-  res.status(200).jsonp(rand)
-})
-
-// app.post('/auth/login', (req, res) => {
-app.post('/login', (req, res) => {
-  const { username, password } = req.body
-  if (isAuthenticated({ username, password }) === false) {
-    const status = 401
-    const message = 'Incorrect username or password'
-    res.status(status).jsonp({ status, message })
-  } else {
-    const access_token = createToken({ username, password })
-    // res.cookie('access_token', access_token)
-    // res.header('Set-Cookie', `access_token=${access_token}`)
-    res.header('Authorization', `Bearer ${access_token}`)
-    // res.status(200).jsonp({ access_token })
-    const rand = Object.assign(jsf.generate(LoginResponseModel), { token: access_token })
-    res.status(200).jsonp(rand)
-  }
-})
 
 app.get('/*', (req, res) => {
   res.status(404).send('Route not found')
