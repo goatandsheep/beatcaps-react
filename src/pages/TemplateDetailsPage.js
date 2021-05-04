0dbefecdadc4e852ae46f5b2382db778710e150b
Linda <lindajzhao@gmail.com>
2021-05-04 14:57:56 -0400

    display view data, add buttons

--- src/pages/TemplateDetailsPage.js
+++ src/pages/TemplateDetailsPage.js
@@ -1,4 +1,5 @@
 import React, {useContext, useEffect, useState} from 'react';
+import {Link} from 'react-router-dom';
 import {GlobalContext} from '../contexts/GlobalState';
 import constants from '../constants';
 
@@ -25,30 +26,62 @@ const TemplateDetailsPage = (props) => {
   return (
     <div>
       <h1 className="title is-2">Template Details</h1>
+      <div className="container">
+        <Link to="file/new" className="button">Edit
+          <span className="icon is-small is-left ml-1">
+            <i className="fas fa-edit"></i>
+          </span>
+        </Link>
+        <Link to="file/new" className="button ml-5 is-primary">Create output from template
+          <span className="icon is-small is-left ml-1">
+            <i className="fas fa-plus"></i>
+          </span>
+        </Link>
+      </div>
       <div className="card card-content content">
         {
           template?.views ?
           (
             <>
               <p className="subtitle is-5 has-text-left mb-0">
-                <label>ID</label>: <strong>{template.id}</strong>
+                <span class="has-text-weight-bold">ID</span>: <span>{template.id}</span>
               </p>
               <p className="subtitle is-5 has-text-left mb-0">
-                <label>Name</label>: <strong>{template.name}</strong>
+                <span class="has-text-weight-bold">Name</span>: <span>{template.name}</span>
               </p>
               <p className="subtitle is-5 has-text-left mb-0">
-                <label>Height in px</label>: <strong>{template.height}</strong>
+                <span class="has-text-weight-bold">Height in px</span>: <span>{template.height}</span>
               </p>
               <p className="subtitle is-5 has-text-left mb-0">
-                <label>Width in px</label>: <strong>{template.width}</strong>
+                <span class="has-text-weight-bold">Width in px</span>: <span>{template.width}</span>
               </p>
               <p className="subtitle is-5 has-text-left mb-0">
-                <label>Views in this template</label>: <strong>{template.views.length}</strong>
+                <span class="has-text-weight-bold">Views in this template</span>: <span>{template.views.length}</span>
               </p>
+
+              <h2 className="title is-4 mt-5">Views:</h2>
+              {template.views.map((view, i) => {
+                return (
+                  <div className="card card-content content mt-5 has-text-left">
+                    <h3 className="title is-5 my-2">View {i + 1}</h3>
+                    <p class="mb-0">
+                      <span class="has-text-weight-bold">Height</span>: <span>{view.height}</span>
+                    </p>
+                    <p class="mb-0">
+                      <span class="has-text-weight-bold">Width</span>: <span>{view.width}</span>
+                    </p>
+                    <p class="mb-0">
+                      <span class="has-text-weight-bold">X</span>: <span>{view.x}</span>
+                    </p>
+                    <p class="mb-0">
+                      <span class="has-text-weight-bold">Y</span>: <span>{view.x}</span>
+                    </p>
+                  </div>
+                );
+              })}
             </>
           ) : <span className="is-loading">Loading</span>
         }
-
       </div>
     </div>
   );
